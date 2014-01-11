73810d1b38168c3d3cbfdad4c0caeb17cacde092
Marcin Bednarek <paws77@gmail.com>
2014-01-11 22:08:43 +0100

    Methods do not throw annotations.

--- src/test/java/org/junit/runner/SingleJUnitTestRunnerTest.java
+++ src/test/java/org/junit/runner/SingleJUnitTestRunnerTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 public class SingleJUnitTestRunnerTest {
 
     @Test
-    public void should_Return_Error_When_Null_Was_Sent() throws Exception {
+    public void should_Return_Error_When_Null_Was_Sent() {
         SingleJUnitTestRunner.main(null); 
     }
 
     @Test
-    public void should_Return_Error_When_Only_Class_Was_Sent() throws Exception {
+    public void should_Return_Error_When_Only_Class_Was_Sent() {
         SingleJUnitTestRunner.main(new String[] {"Test"}); 
     }
 }
